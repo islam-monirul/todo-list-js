eed93e4ab6ca4ed097229d34b39cb6d8abf716b9
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-08-07 13:34:35 +0600

    adding to list work done

--- todo.js
+++ todo.js
@@ -1,5 +1,18 @@
+// const listItems = document.querySelectorAll('li');
+// listItems.forEach(item => {
+//      item.addEventListener('click', e => {
+          
+//      });
+// });
+
+const ul = document.querySelector('ul');
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
      const text = document.querySelector('input').value;
-     console.log(text);
+     
+     const li = document.createElement('li');
+     li.textContent = `${text}`;
+
+     ul.append(li);
 });
